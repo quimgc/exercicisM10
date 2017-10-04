d72f9f4a7295180fae195b6dc35bc8f18ea4b6fb
quimgc <quimgonzalez@iesebre.com>
2017-10-04 17:18:35 +0200

    Projecte millora d'hores amb tarifa

--- primerProjectePython/src/funcions.py
+++ primerProjectePython/src/funcions.py
@@ -63,7 +63,7 @@ def mcd3 (a, b, c):
 def delReves(num):
     longitud = len(str(num))
     
-    base = 10 ** (longitud -1) # lki resto 1 a longitud perquè sino me donaria 1000, d'aquesta forma me dona 100
+    base = 10 ** (longitud -1) # li resto 1 a longitud perquè sino me donaria 1000, d'aquesta forma me dona 100
    
     numReves = 0
     while base >= 1:
@@ -77,5 +77,30 @@ def delReves(num):
     return int(numReves)
 
 
-print (delReves(12345453))
+def delRevesv2(num):
+    
+    #declaro una variable base per multiplicar els digits.
+    base = 1
+    numInvertit = 0 # variable que retornaré amb la cadena invertida.
+    numString = str(num) # converteixo la variable passada per paràmetre a String
+    
+    #recorro un for per tenir cada digit.
+    for numero in numString:
+        #multiplico 'numero' * base
+        numInvertit = numInvertit + int(numero) * base
+        #incremento base multiplicada x 10
+        base = base * 10
+    #retorno el num invertit
+    return numInvertit
+
+
+def delRevesv3(num):
+    
+    # per utilitzar [::-1] (això el que fa és recorrer l'string a la inversa) es necessita que sigui de tipus String.
+    
+    return str(num)[::-1]
+
+
+
+
 
